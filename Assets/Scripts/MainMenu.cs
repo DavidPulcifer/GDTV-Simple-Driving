e87c383aa8d1747c1b4362af20fdf8a2012cd89d
David Pulcifer <thalliosqueldaer@gmail.com>
2021-07-02 22:49:57 -0700

    Final Lecture Commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] TMP_Text highScoreText;
+    [SerializeField] TMP_Text energyText;
+    [SerializeField] Button playButton;
+    [SerializeField] AndroidNotificationHandler androidNotificationHandler;
+    [SerializeField] int maxEnergy;
+    [SerializeField] int energyRechargeDuration;
+
+    int energy;
+
+    const string EnergyKey = "Energy";
+    const string EnergyReadyKey = "EnergyReady";
+
+    private void Start() 
+    {
+        int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
+
+        highScoreText.text = $"High Score: {highScore}";
+
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
+
+        if(energy == 0) 
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+
+            if(energyReadyString == string.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if(DateTime.Now > energyReady) 
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy);
+            } 
+            else 
+            {
+                playButton.interactable = false;
+                Invoke(nameof(EnergyRecharged), (energyReady - DateTime.Now).Seconds);
+            }
+        }
+
+        energyText.text = $"Play ({energy})";
+    }
+
+    void EnergyRecharged()
+    {
+        playButton.interactable = true;
+        energy = maxEnergy;
+        PlayerPrefs.SetInt(EnergyKey, energy);
+        energyText.text = $"Play ({energy})";
+    }
+    
+    public void Play()
+    {
+        if(energy < 1) return;
+
+        energy--;
+
+        PlayerPrefs.SetInt(EnergyKey, energy);
+
+        if(energy < 1)
+        {
+            DateTime energyRefreshTime = DateTime.Now.AddMinutes(energyRechargeDuration);
+            PlayerPrefs.SetString(EnergyReadyKey, energyRefreshTime.ToString());
+#if UNITY_ANDROID
+            androidNotificationHandler.ScheduleNotification(energyRefreshTime);
+#endif
+
+        }
+        SceneManager.LoadScene(1);
+    }
+}
